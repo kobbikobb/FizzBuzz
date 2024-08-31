6652c7f8b408589a91af9e3c4ed079215a5725c9
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-08-31 17:27:08 +0000

    newline

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,6 +5,7 @@ function doFizzBuzz() {
 
 function getFizzBuzz() {
     const arr = [];
+
     for(var i=1; i<=100; i++) {
         if(i % 3 === 0 && i % 5 === 0) {
             arr.push('FizzBuzz');
